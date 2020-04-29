15fcd86d7d6f3013dc1545e814fb12dd428b7d08
milos-andric <55629183+milos-andric@users.noreply.github.com>
2020-04-29 16:14:43 +0200

    norming - draw_circle

--- srcs/Canvas/circle.c
+++ srcs/Canvas/circle.c
@@ -6,55 +6,59 @@
 /*   By: milosandric <milosandric@student.42lyon    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 16:58:38 by siferrar          #+#    #+#             */
-/*   Updated: 2020/04/29 15:58:28 by milosandric      ###   ########lyon.fr   */
+/*   Updated: 2020/04/29 16:13:48 by milosandric      ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../../includes/my_canvas.h"
 
+void    draw_outer()
+{
+	pixel_put(c_x + x, c_y + y, ctx->color, ctx->cur_buff);
+	pixel_put(c_x + y, c_y + x, ctx->color, ctx->cur_buff);
+	pixel_put(c_x - x, c_y + y, ctx->color, ctx->cur_buff);
+	pixel_put(c_x - y, c_y + x, ctx->color, ctx->cur_buff);
+	pixel_put(c_x + x, c_y - y, ctx->color, ctx->cur_buff);
+	pixel_put(c_x + y, c_y - x, ctx->color, ctx->cur_buff);
+	pixel_put(c_x - x, c_y - y, ctx->color, ctx->cur_buff);
+	pixel_put(c_x - y, c_y - x, ctx->color, ctx->cur_buff);    
+}
+
 void	draw_circle(float c_x, float c_y, int ray, int fill, t_ctx *ctx)
 {
-    int          x;
-    int          y;
-    int          d;
-    
-    while ((fill == 1 && ray > 0) || fill == 0)
-    {
-        x = 0;
-        y = ray;
-        d = ray - 1;
-        while(y >= x)
-        {
-            pixel_put(c_x + x, c_y + y, ctx->color, ctx->cur_buff);
-            pixel_put(c_x + y, c_y + x, ctx->color, ctx->cur_buff);
-            pixel_put(c_x - x, c_y + y, ctx->color, ctx->cur_buff);
-            pixel_put(c_x - y, c_y + x, ctx->color, ctx->cur_buff);
-            pixel_put(c_x + x, c_y - y, ctx->color, ctx->cur_buff);
-            pixel_put(c_x + y, c_y - x, ctx->color, ctx->cur_buff);
-            pixel_put(c_x - x, c_y - y, ctx->color, ctx->cur_buff);
-            pixel_put(c_x - y, c_y - x, ctx->color, ctx->cur_buff);
-            
-            if (d >= 2 * x)
-            {
-                d -= 2 * x + 1;
-                x++;
-            }
-            else if (d < 2 * (ray - y))
-            {
-                d += 2 * y - 1;
-                y--;
-            }
-            else
-            {
-                d += 2 * (y - x - 1);
-                y--;
-                x++;
-            }
-        }
-        if (!fill)
-            return ;
-        ray--;
-    }
-    pixel_put(c_x, c_y, ctx->color, ctx->cur_buff);
+	int          x;
+	int          y;
+	int          d;
+	
+	while ((fill == 1 && ray > 0) || fill == 0)
+	{
+		x = 0;
+		y = ray;
+		d = ray - 1;
+		while(y >= x)
+		{
+			draw_outer(c_x, c_y, x, y, ctx)
+			if (d >= 2 * x)
+			{
+				d -= 2 * x + 1;
+				x++;
+			}
+			else if (d < 2 * (ray - y))
+			{
+				d += 2 * y - 1;
+				y--;
+			}
+			else
+			{
+				d += 2 * (y - x - 1);
+				y--;
+				x++;
+			}
+		}
+		if (!fill)
+			return ;
+		ray--;
+	}
+	pixel_put(c_x, c_y, ctx->color, ctx->cur_buff);
 }
